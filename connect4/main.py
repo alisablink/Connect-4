bc80050d284d48a4de8d4686dd02b66f2ef73ede
alisablink <alisablue@protonmail.com>
2022-11-26 00:17:50 +0300

    Victory check

--- connect4/main.py
+++ connect4/main.py
@@ -8,16 +8,43 @@ def create_board():
     return board
 
 def drop_piece(col,player):
-    board[get_next_open_row(board, col), col] = player
-    pass
+    row = get_next_open_row(board, col)
+    board[row, col] = player
+    result = check_victory(board, row, col)
+    return result
 
 def is_valid_location(board, col):
-    return board[0][col] == 0
+    return col in range(7) and board[0][col] == 0
 
 def get_next_open_row(board,col):
     for r in range(ROW_COUNT-1,-1,-1):
         if board[r][col] == 0:
             return r
+
+def check_victory(board, row, col):
+    player1win = 0
+    player2win = 0
+    for r in range(6): # Check the row 
+        if board[col][r] == 1:
+            player1win += 1
+            player2win = 0
+            if player1win == 4: return 1
+        elif board[r][col] == 2:
+            player1win = 0
+            player2win += 1
+            if player2win == 4: return 2
+    player1win = 0
+    player2win = 0
+    for r in range(6): # Check the column
+        if board[r][col] == 1:
+            player1win += 1
+            player2win = 0
+            if player1win == 4: return 1
+        elif board[r][col] == 2:
+            player1win = 0
+            player2win += 1
+            if player2win == 4: return 2
+    
 
 board = create_board()
 print(board)
@@ -34,7 +61,17 @@ while not game_over:
     else:
         col = int(input("Player 2 Make your Selection (0-6):"))
     if is_valid_location(board, col):
-        drop_piece(col, turn+1)
+        result = drop_piece(col, turn+1)
+        if result == 1:
+            print(board)
+            print("Player 1 has won!")
+            game_over = True
+            break
+        elif result == 2:
+            print(board)
+            print("Player 2 has won!")
+            game_over = True
+            break
         turn += 1
         turn = turn % 2
         print(board)
